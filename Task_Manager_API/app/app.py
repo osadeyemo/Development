3f16cfcc21fb4349c81b9d2f3ff33f97c2af3a0f
osadeyemo <osadeyemo@yahoo.com>
2026-04-02 10:47:23 -0500

    updated code

--- Task_Manager_API/app/app.py
+++ Task_Manager_API/app/app.py
@@ -1,11 +1,30 @@
-import os
-from flask import Flask
+from flask import Flask, jsonify
 import psycopg2
+import os
+
 app = Flask(__name__)
+
+def get_db_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
+    )
 
 @app.route("/")
 def home():
-    return "Task Manager API Running Successfully!"
+    return "E-commerce API running"
+
+@app.route("/products")
+def get_products():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT NOW();")
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    return jsonify({"time": result[0]})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
